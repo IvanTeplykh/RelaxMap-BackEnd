a2ff21d81acc4a8cfcdc7186d303bbd0a3282d9f
Polina Matviienko <polinamatviienko030@gmail.com>
2026-06-05 23:36:26 +0300

    add feedbacks public GET and private POST endpoints

--- src/models/location.js
+++ src/models/location.js
@@ -1,5 +1,45 @@
 import { Schema, model } from "mongoose";
 
-const locationShema = new Schema({});
+const locationSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+    },
+    locationType: {
+      type: String,
+    },
+    region: {
+      type: String,
+    },
+    rate: {
+      type: Number,
+    },
+    description: {
+      type: String,
+    },
+    coordinates: {
+      lat: { type: Number },
+      lon: { type: Number },
+    },
+    ownerId: {
+      type: Schema.Types.ObjectId,
+      ref: "user",
+    },
+    feedbacksId: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "feedback",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
 
-export const LocationModel = model("Location", locationShema);
+export const LocationModel = model("location", locationSchema);
